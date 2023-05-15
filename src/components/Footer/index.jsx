0ff84561eba90b879f06f0d31b0e8fe46e367b62
markliu2013 <markliu2013@gmail.com>
2023-05-15 13:57:51 +0800

    save

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -20,7 +20,7 @@ export default () => {
         <Divider type="vertical" />
         <a target="_blank" href="https://beian.miit.gov.cn/" rel="noreferrer">{t('footer.no')}</a>
         <Divider type="vertical" />
-        v1.0.12
+        v1.0.13
       </div>
       <ModalContainer />
     </Space>
