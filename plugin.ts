7915e59ae6f9ee1f2d69b6c8186e69a01e7de730
markliu2013 <markliu2013@gmail.com>
2023-05-28 10:10:18 +0800

    add baidu tongji

--- plugin.ts
+++ plugin.ts
@@ -5,6 +5,18 @@ export default (api: IApi) => {
     $('head').append([
       `<link href="/favicon.ico" rel="shortcut icon">`,
     ])
+    // 百度统计
+    $('head').append([
+      `<script>
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?c612aa29079f1e51c734eea7401c215c";
+          var s = document.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(hm, s);
+        })();
+      </script>`,
+    ])
     return $;
   });
 };
