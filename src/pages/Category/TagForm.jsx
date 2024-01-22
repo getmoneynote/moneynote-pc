7103330387beea19dfaf81ebb4a48e1d71a9ec3f
markliu2013 <markliu2013@gmail.com>
2024-01-22 10:19:56 +0800

    save

--- src/pages/Category/TagForm.jsx
+++ src/pages/Category/TagForm.jsx
@@ -66,7 +66,7 @@ export default () => {
       initialValues={initialValues}
     >
       <ProFormTreeSelect
-        name="pId"
+        name="parent"
         label={t('label.parent.tag')}
         fieldProps={{
           ...treeSelectSingleProp,
